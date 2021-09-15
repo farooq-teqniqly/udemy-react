4ed8413fc59e331b88ecca20ada285d873327e1f
Farooq Mahmud <farooq.mahmud@marel.com>
2021-09-15 15:53:28 -0700

    initial commit

---
+++ rcg/apps/react-complete-guide/src/app/app.tsx
@@ -0,0 +1,10 @@
+import styles from './app.module.css';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export function App() {
+  return <div></div>;
+}
+
+export default App;
