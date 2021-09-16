29c7e667e8afceffd2680a1c11e8b1eb6f098f4a
Farooq Mahmud <farooq.mahmud@marel.com>
2021-09-16 15:28:31 -0700

    wip

--- rcg/libs/expenses/src/lib/expense-item/expense-item.tsx
+++ rcg/libs/expenses/src/lib/expense-item/expense-item.tsx
@@ -1,17 +1,17 @@
-import './expense-item.module.css';
+import styles from './expense-item.module.css';
 
 /* eslint-disable-next-line */
 export interface ExpenseItemProps {}
 
 export function ExpenseItem(props: ExpenseItemProps) {
   return (
-    <>
-      <div>Date</div>
+    <div className={styles['expense-item']}>
+      <div>September 15, 2021</div>
       <div>
-        <h2>Title</h2>
+        <h2 className={styles['expense-item__description']}>Car Insurance</h2>
+        <div className={styles['expense-item__price']}>$65.89</div>
       </div>
-      <div>Amount</div>
-    </>
+    </div>
   );
 }
 
